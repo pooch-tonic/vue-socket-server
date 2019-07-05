c8f3ea041eb063c032e17f29b0c77f7c3f62c592
Ryo SHIINA <ryo.shiina@viacesi.fr>
2019-07-05 17:54:07 +0200

    saving commit

--- index.js
+++ index.js
@@ -8,12 +8,28 @@ const socket = io(http)
 
 socket.on('connection', socket => {
   console.log('user connected')
-  socket.emit('request', '[' + new Date().toTimeString().slice(0, 8) + '] You are now connected to the server.')
+  socket.emit('message', '[' + new Date().toTimeString().slice(0, 8) + '] You are now connected to the server.')
 
-  const socketIntervalId = setInterval(() => {
-    const dateString = '[' + new Date().toTimeString().slice(0, 8) + ']'
-    socket.emit('request', dateString + ' A random message with a new random number: ' + Math.floor(Math.random() * 1000))
-  }, 2000)
+  socket.on('start-operation', () => {
+    console.log('user started an operation')
+
+    let progress = 0
+    const intervalId = setInterval(() => {
+      progress += Math.floor(Math.random() * 20)
+      
+      if (progress >= 100) {
+        progress = 100
+        socket.emit('message', '[' + new Date().toTimeString().slice(0, 8) + '] Operation in progress... 100%')
+        socket.emit('finalize-operation')
+      }
+      socket.emit('message', '[' + new Date().toTimeString().slice(0, 8) + '] Operation in progress... ' + progress.toString().padStart(3, ' ') + '%')
+    }, 500)
+
+    socket.on('end-operation', () => {
+      clearInterval(intervalId)
+      console.log('user ended the operation')
+    })
+  })
   
   socket.on('disconnect', () => {
     console.log('user disconnected')
